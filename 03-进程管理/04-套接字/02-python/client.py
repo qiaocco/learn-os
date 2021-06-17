da4af0b7a3b310b65f3f5a7d4e0f81fa44d9e133
qiaocc <qiaocco@gmail.com>
2021-06-17 23:54:06 +0800

    修改 socketserver

--- 03-进程管理/04-套接字/02-python/client.py
+++ 03-进程管理/04-套接字/02-python/client.py
@@ -1,9 +1,18 @@
 import socket  # 导入 socket 模块
 
-s = socket.socket()  # 创建 socket 对象
-host = socket.gethostname()  # 获取本地主机名
+host = "127.0.0.1"  # 获取本地主机名
 port = 12345  # 设置端口号
 
-s.connect((host, port))
-print(s.recv(1024).decode())
-s.close()
+
+def main():
+    s = socket.socket()  # 创建 socket 对象
+
+    s.connect((host, port))
+    s.sendall("hello, I am client".encode())
+    print(s.recv(1024).decode())
+
+    s.close()
+
+
+if __name__ == '__main__':
+    main()
